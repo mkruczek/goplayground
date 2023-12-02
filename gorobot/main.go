b61ee1d2409fd215bcfc0ab38bb5a04639d956ac
kruczek <kruczek86@gmail.com>
2023-12-02 17:56:17 +0100

    add turn off and goroutines

--- gorobot/main.go
+++ gorobot/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-vgo/robotgo"
 	"time"
 )
 
 func main() {
-	for {
-		x, y := robotgo.Location()
-		robotgo.Move(x+1, y+1)
-		time.Sleep(60 * time.Second)
-	}
+
+	go func() {
+		for {
+			x, y := robotgo.Location()
+			robotgo.Move(x+1, y+1)
+			time.Sleep(60 * time.Second)
+		}
+	}()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			x, y := robotgo.Location()
+			//fmt.Println("x:", x, "y:", y)
+			//time.Sleep(1 * time.Second)
+			if x == 0 && y == 1079 {
+				fmt.Println("done")
+				done <- true
+			}
+		}
+	}()
+
+	<-done
+
 }
