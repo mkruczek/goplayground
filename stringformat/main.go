f73b9f633e4d5a7d38837a50b8e69d3e7f56a997
kruczek <kruczek86@gmail.com>
2023-12-07 20:24:29 +0100

    simple example of formatting

---
+++ stringformat/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x uint8 = 32
+
+	fmt.Printf("x=%d // decimal\n", x)
+	fmt.Printf("x=%b // binary \n", x)
+	fmt.Printf("x=%o // octal \n", x)
+	fmt.Printf("x=%x // hexadecimal \n", x)
+	fmt.Printf("x=%X // hexadecimal \n", x)
+
+	fmt.Printf("x=%c // char \n", x)
+	fmt.Printf("x=q%q // quoted char \n", x)
+}
