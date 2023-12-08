68a319c40c0e4b8875943a8136e8c26435d1965f
kruczek <kruczek86@gmail.com>
2023-12-08 21:52:36 +0100

    remove comments

--- scraper/main.go
+++ scraper/main.go
@@ -8,12 +8,10 @@ import (
 )
 
 func main() {
-	// Tworzymy nową instancję Colly
 	c := colly.NewCollector(
 		colly.AllowedDomains("scrapethissite.com", "www.scrapethissite.com"),
 	)
 
-	// Ustawiamy akcję dla każdego odwiedzonego elementu
 	c.OnHTML(".col-md-4.col-sm-6.col-xs-12", func(e *colly.HTMLElement) {
 		title := e.ChildText("h3")
 		description := e.ChildText("p")
@@ -21,12 +19,10 @@ func main() {
 		fmt.Printf("Title: %s\nDescription: %s\n\n", title, description)
 	})
 
-	// Obsługa błędów
 	c.OnError(func(r *colly.Response, err error) {
 		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
-	// Rozpoczęcie skrapowania
 	err := c.Visit("https://www.scrapethissite.com/pages/")
 	if err != nil {
 		log.Println("Error visiting page:", err)
