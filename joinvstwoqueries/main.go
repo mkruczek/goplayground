85ffa435bc634c50c5ea50a466be5b8fd7064b17
kruczek <kruczek86@gmail.com>
2023-12-27 21:52:58 +0100

    improve go code

--- joinvstwoqueries/main.go
+++ joinvstwoqueries/main.go
@@ -13,6 +13,7 @@ type Invoice struct {
 	ID         int
 	CustomerID int
 	Date       string
+	Details    []InvoiceDetails
 }
 
 type InvoiceDetails struct {
@@ -35,14 +36,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	invoices := make([]Invoice, 0)
+	invoicesTwoQ := make([]Invoice, 0)
 	for rows.Next() {
 		var invoice Invoice
 		err = rows.Scan(&invoice.ID, &invoice.CustomerID, &invoice.Date)
 		if err != nil {
 			log.Fatal(err)
 		}
-		invoices = append(invoices, invoice)
 
 		details, err := db.Query("SELECT * FROM invoice_details WHERE invoice_id = $1", invoice.ID)
 		if err != nil {
@@ -58,27 +58,37 @@ func main() {
 			invoiceDetails = append(invoiceDetails, detail)
 		}
 		details.Close()
+
+		invoice.Details = invoiceDetails
+		invoicesTwoQ = append(invoicesTwoQ, invoice)
 	}
 	rows.Close()
-	fmt.Printf("Time taken for two queries: %v.Got %d invoces.\n", time.Since(start), len(invoices))
+	fmt.Printf("Time taken for two queries: %v.Got %d invoces.\n", time.Since(start), len(invoicesTwoQ))
 
 	// join query
 	start = time.Now()
-	rows, err = db.Query("SELECT invoice.id, invoice.customer_id, invoice.date FROM invoice JOIN invoice_details ON invoice.id = invoice_details.invoice_id GROUP BY invoice.id")
+	rows, err = db.Query("SELECT invoice.id, invoice.customer_id, invoice.date, invoice_details.id, invoice_details.invoice_id, invoice_details.product_id, invoice_details.quantity FROM invoice JOIN invoice_details ON invoice.id = invoice_details.invoice_id")
 	if err != nil {
 		log.Fatal(err)
 	}
-	invoices = make([]Invoice, 0)
+	invoicesJ := make(map[int]Invoice)
 	for rows.Next() {
 		var invoice Invoice
-		err = rows.Scan(&invoice.ID, &invoice.CustomerID, &invoice.Date)
+		var detail InvoiceDetails
+		err = rows.Scan(&invoice.ID, &invoice.CustomerID, &invoice.Date, &detail.ID, &detail.InvoiceID, &detail.ProductID, &detail.Quantity)
 		if err != nil {
 			log.Fatal(err)
 		}
-		invoices = append(invoices, invoice)
+		if existingInvoice, ok := invoicesJ[invoice.ID]; ok {
+			existingInvoice.Details = append(existingInvoice.Details, detail)
+			invoicesJ[invoice.ID] = existingInvoice
+		} else {
+			invoice.Details = append(invoice.Details, detail)
+			invoicesJ[invoice.ID] = invoice
+		}
 	}
 	rows.Close()
-	fmt.Printf("Time taken for join query: %v. Got %d invoices\n", time.Since(start), len(invoices))
+	fmt.Printf("Time taken for join query: %v. Got %d invoices\n", time.Since(start), len(invoicesJ))
 
 	db.Close()
 }
