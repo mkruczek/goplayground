9d39c060c4ca5cdcdc3b5e37b2ada06f43374b07
kruczek <kruczek86@gmail.com>
2023-12-29 22:48:47 +0100

    divide sql and values

--- query/query.go
+++ query/query.go
@@ -6,13 +6,15 @@ import (
 	"strings"
 )
 
-func buildSQLQuery(params url.Values) string {
+func buildSQLQuery(params url.Values) (string, []any) {
 	sqlQuery := "SELECT * FROM table"
+	var args []interface{}
 
 	whereClause := make([]string, 0)
 	for key, values := range params {
 		if strings.HasPrefix(key, "where") {
-			whereClause = append(whereClause, fmt.Sprintf("%s = '%s'", strings.TrimPrefix(key, "where_"), values[0]))
+			whereClause = append(whereClause, fmt.Sprintf("%s = ?", strings.TrimPrefix(key, "where_")))
+			args = append(args, values[0])
 		}
 	}
 
@@ -24,7 +26,7 @@ func buildSQLQuery(params url.Values) string {
 		sqlQuery = fmt.Sprintf("%s ORDER BY %s", sqlQuery, sort[0])
 	}
 
-	return sqlQuery
+	return sqlQuery, args
 }
 
 func main() {
@@ -37,6 +39,7 @@ func main() {
 
 	params := parsedURL.Query()
 
-	sqlQuery := buildSQLQuery(params)
+	sqlQuery, args := buildSQLQuery(params)
 	fmt.Println(sqlQuery)
+	fmt.Println(args)
 }
