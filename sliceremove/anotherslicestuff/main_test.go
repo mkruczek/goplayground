6fe049bef19951ba04ada6ada95046544df4102f
kruczek <kruczek86@gmail.com>
2024-01-26 13:09:11 +0100

    fancy way to create new slice

---
+++ sliceremove/anotherslicestuff/main_test.go
@@ -0,0 +1,13 @@
+package anotherslicestuff
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_myTest(t *testing.T) {
+
+	x := append([]string{}, "fancy way to create new slice :D")
+
+	fmt.Printf("%v\n", x)
+}
